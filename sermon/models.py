074f3a0f853569351433297c807741e523fc3da5
Renan Noronha <renancnoronha@gmail.com>
2022-03-01 09:37:25 -0300

    added thumbnail and ativo to sermon model

--- sermon/models.py
+++ sermon/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Sermon(models.Model):
+    thumbnail = models.ImageField('Thumbnail', upload_to='sermon/')
     title = models.CharField('Título', max_length=255)
     speaker = models.CharField('Orador', max_length=255)
     date = models.DateField('Data')
     description = models.TextField('Descrição', null=True, blank=True)
     link = models.CharField('Link', max_length=1024)
+    ativo = models.BooleanField('Ativo', default=True, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Sermão'
