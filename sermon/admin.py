68a1c5e225be34fb21d3cfb87b27409ea7c6073c
Renan Noronha <renancnoronha@gmail.com>
2022-03-01 09:33:21 -0300

    sermon model

--- sermon/admin.py
+++ sermon/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Sermon)
+class SermonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'speaker', 'date']
+    ordering = ['-date']
--- sermon/models.py
+++ sermon/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Sermon(models.Model):
+    title = models.CharField('Título', max_length=255)
+    speaker = models.CharField('Orador', max_length=255)
+    date = models.DateField('Data')
+    description = models.TextField('Descrição', null=True, blank=True)
+    link = models.CharField('Link', max_length=1024)
+
+    class Meta:
+        verbose_name = 'Sermão'
+        verbose_name_plural = 'Sermões'
