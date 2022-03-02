bff4ce55c041181993738740911c323763307a9b
Renan Noronha <renancnoronha@gmail.com>
2022-03-02 09:23:04 -0300

    about route and view

---
+++ about/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('about', AboutView.as_view(), name='about'),
+]
--- about/views.py
+++ about/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from home.views import BaseView
 
 # Create your views here.
+class AboutView(BaseView):
+    template_name = 'about/about.html'
+    title = 'Sobre'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        context['title'] = self.title + ' - ' + context['config'].nome
+        return render(request, self.template_name, context)
