22384e7cc1ef570152bfa03d03a402909fa95b73
Renan Noronha <renancnoronha@gmail.com>
2022-03-02 11:34:37 -0300

    added button to pull videos from youtube on demand

--- sermon/admin.py
+++ sermon/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django.contrib import messages
+from django_object_actions import DjangoObjectActions
 from .models import *
+
+from .views import getNewVideos
 
 # Register your models here.
 @admin.register(Sermon)
-class SermonAdmin(admin.ModelAdmin):
+class SermonAdmin(DjangoObjectActions, admin.ModelAdmin):
     list_display = ['title', 'speaker', 'date', 'ativo']
     ordering = ['-date']
+    
+    def importar_videos_novos(modeladmin, request, queryset):
+        try:
+            getNewVideos()
+            messages.success(request, "Lista de Vídeos atualizada com sucesso!")
+        except Exception as e:
+            messages.error(request, "Erro ao buscar os vídeos no Youtube.")
+
+    importar_videos_novos.label = "Impotar Vídeos Novos"
+    importar_videos_novos.short_description = "Puxar vídeos novos do Youtube automaticamente"
+
+    changelist_actions = ('importar_videos_novos', )
--- sermon/views.py
+++ sermon/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .models import Sermon
+
+from apiclient.discovery import build
+import requests
+from datetime import datetime
 
 # Create your views here.
+def getNewVideos():
+    api_key = open("api_key.txt", "r").read()
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    channel_id = 'UCsLvHS14G27MukIDiVDgiWQ'
+
+    response = youtube.channels().list(id=channel_id, part='contentDetails').execute()
+    playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    response = youtube.playlistItems().list(playlistId=playlist_id, part='snippet').execute()
+
+    lastVideo = Sermon.objects.all().order_by('-date').first()
+    for video in response['items']:
+        date = datetime.strptime(video['snippet']['publishedAt'][:-1], '%Y-%m-%dT%H:%M:%S').date()
+        if date == lastVideo.date:
+            print(date)
+            print(lastVideo.date)
+            break
+        elif date > lastVideo.date:
+            img_data = requests.get(video['snippet']['thumbnails']['maxres']['url']).content
+            file_path = 'sermon/' + video['snippet']['resourceId']['videoId'] + '.jpg'
+            with open('media/' + file_path, 'wb') as handler:
+                handler.write(img_data)
+
+            speaker = video['snippet']['title'].split(' - ')[1] if ' - ' in video['snippet']['title'] else 'Placeholder'
+            Sermon.objects.create(
+                thumbnail=file_path,
+                title=video['snippet']['title'],
+                speaker=speaker,
+                date=date,
+                description=video['snippet']['description'],
+                link='https://www.youtube.com/watch?v=' + video['snippet']['resourceId']['videoId'],
+                ativo=False,
+            )
