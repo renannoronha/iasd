7910dba346023233c3a289fb9a6d9c86ac0115e8
Renan Noronha <renancnoronha@gmail.com>
2022-03-08 08:03:39 -0300

    fixed maxres error

--- sermon/views.py
+++ sermon/views.py
@@ -13,38 +13,44 @@ import os
 
 # Create your views here.
 def getNewVideos():
-    api_key = open(os.path.join(settings.BASE_DIR, "api_key.txt"), "r").read()
-    youtube = build('youtube', 'v3', developerKey=api_key)
-    channel_id = 'UCsLvHS14G27MukIDiVDgiWQ'
+    try:
+        api_key = open(os.path.join(settings.BASE_DIR, "api_key.txt"), "r").read()
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel_id = 'UCsLvHS14G27MukIDiVDgiWQ'
 
-    response = youtube.channels().list(id=channel_id, part='contentDetails').execute()
-    playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-    response = youtube.playlistItems().list(playlistId=playlist_id, part='snippet').execute()
+        response = youtube.channels().list(id=channel_id, part='contentDetails').execute()
+        playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+        response = youtube.playlistItems().list(playlistId=playlist_id, part='snippet').execute()
 
-    lastVideo = Sermon.objects.all().order_by('-date').first()
-    for video in response['items']:
-        date = datetime.strptime(video['snippet']['publishedAt'][:-1], '%Y-%m-%dT%H:%M:%S').date()
-        if date == lastVideo.date:
-            print(date)
-            print(lastVideo.date)
-            break
-        elif date > lastVideo.date:
-            img_data = requests.get(video['snippet']['thumbnails']['maxres']['url']).content
-            file_path = 'sermon/' + video['snippet']['resourceId']['videoId'] + '.jpg'
-            with open('media/' + file_path, 'wb') as handler:
-                handler.write(img_data)
+        lastVideo = Sermon.objects.all().order_by('-date').first()
+        for video in response['items']:
+            date = datetime.strptime(video['snippet']['publishedAt'][:-1], '%Y-%m-%dT%H:%M:%S').date()
+            if date == lastVideo.date:
+                print(date)
+                print(lastVideo.date)
+                break
+            elif date > lastVideo.date:
+                try:
+                    img_data = requests.get(video['snippet']['thumbnails']['maxres']['url']).content
+                except Exception as e:
+                    img_data = requests.get(video['snippet']['thumbnails']['high']['url']).content
+                file_path = 'sermon/' + video['snippet']['resourceId']['videoId'] + '.jpg'
+                with open('media/' + file_path, 'wb') as handler:
+                    handler.write(img_data)
 
-            speaker = video['snippet']['title'].split(' - ')[1] if ' - ' in video['snippet']['title'] else 'Placeholder'
-            Sermon.objects.create(
-                thumbnail=file_path,
-                title=video['snippet']['title'],
-                speaker=speaker,
-                date=date,
-                description=video['snippet']['description'],
-                link='https://www.youtube.com/watch?v=' +
-                video['snippet']['resourceId']['videoId'],
-                ativo=False,
-            )
+                speaker = video['snippet']['title'].split(' - ')[1] if ' - ' in video['snippet']['title'] else 'Placeholder'
+                Sermon.objects.create(
+                    thumbnail=file_path,
+                    title=video['snippet']['title'],
+                    speaker=speaker,
+                    date=date,
+                    description=video['snippet']['description'],
+                    link='https://www.youtube.com/watch?v=' +
+                    video['snippet']['resourceId']['videoId'],
+                    ativo=False,
+                )
+    except Exception as e:
+        print(e)
 
 
 class SermonView(BaseView):
