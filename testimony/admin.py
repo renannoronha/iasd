bb2fc9d5896921c4a71eb005e2ff2383b8c19ccd
Renan Noronha <renancnoronha@gmail.com>
2022-03-10 08:32:27 -0300

    testemunho -> testimony

--- testimony/admin.py
+++ testimony/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-@admin.register(testimony)
-class testimonyAdmin(admin.ModelAdmin):
+@admin.register(Testimony)
+class TestimonyAdmin(admin.ModelAdmin):
     list_display = ['name']
--- testimony/models.py
+++ testimony/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class testimony(models.Model):
+class Testimony(models.Model):
     image = models.ImageField('Image', upload_to='testimony/')
     name = models.CharField('Nome', max_length=255)
     shortDescription = models.CharField('Descrição Curta', max_length=255)
-    description = models.TextField('testimony')
-    video = models.CharField('URL do vídeo do testimony (Youtube)', max_length=255, null=True, blank=True)
+    description = models.TextField('Testemunho')
+    video = models.CharField('URL do vídeo do Testemunho (Youtube)', max_length=255, null=True, blank=True)
