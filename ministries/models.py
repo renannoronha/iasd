8c108f9f306e09d76f4fabb79200cfa625edefd4
Renan Noronha <renancnoronha@gmail.com>
2022-03-10 09:03:09 -0300

    ministries page

--- ministries/models.py
+++ ministries/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Ministry(models.Model):
+    image = models.ImageField('Imagem', upload_to='ministry/')
+    title = models.CharField('Nome do Ministério', max_length=255)
+    shortDescription = models.CharField('Descrição Curta', max_length=255)
+    about = models.TextField('Sobre o Ministério', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Ministério'
+        verbose_name_plural = 'Ministérios'
--- ministries/views.py
+++ ministries/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from home.views import BaseView
+from .models import Ministry
 
 # Create your views here.
+class MinistriesView(BaseView):
+    template_name = 'ministries/ministries.html'
+    title = 'Ministérios'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        context['title'] = self.title + ' - ' + context['config'].nome
+
+        paginator = Paginator(Ministry.objects.all(), 6)
+        page = request.GET.get('p', 1)
+        try:
+            ministries = paginator.page(page)
+        except PageNotAnInteger:
+            ministries = paginator.page(1)
+        except EmptyPage:
+            ministries = paginator.page(paginator.num_pages)
+
+        context['ministries'] = ministries
+        return render(request, self.template_name, context)
