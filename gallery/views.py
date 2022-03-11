83d23dc7b86e6d19079e9399789fe00bc6b083f6
Renan Noronha <renancnoronha@gmail.com>
2022-03-11 08:15:26 -0300

    gallery page

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Gallery
+
+from home.views import BaseView
+
 # Create your views here.
+class HomeView(BaseView):
+    template_name = 'gallery/gallery.html'
+    title = 'Galeria de Fotos'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        context['title'] = self.title + ' - ' + context['config'].nome
+        context['gallery'] = Gallery.objects.filter(ativo=True)
+        return render(request, self.template_name, context)
